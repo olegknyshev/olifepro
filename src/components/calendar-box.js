ef602d1af25da04d2f628515ec5524ab38c565e7
olegknyshev <knyschev@gmail.com>
2020-04-29 17:42:41 +0300

    calendar

--- src/components/calendar-box.js
+++ src/components/calendar-box.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-const CalendarBox = ({myBe, myDate}) => {
-
-  let date = [];
-  date[0] = new Date(myDate-(24*60*60*1000*58));  
-  date[1] = new Date(myDate-(24*60*60*1000*25));
-  date[2] = new Date(myDate-(24*60*60*1000));
-  date[3] = new Date(myDate+(24*60*60*1000*32));
+const CalendarBox = ({myBe, myDate}) => {  
 
   const options = {    
     // year: 'numeric',
-    // month: 'numeric',
+    month: 'short',
     day: 'numeric',   
   };
 
@@ -22,9 +16,8 @@ const CalendarBox = ({myBe, myDate}) => {
     return item;
   }); 
 
-  const monthItem = (numberMonth) => {
-    let a = myBe.filter(item => {let d = new Date(item.date); return d.getMonth() === date[numberMonth].getMonth()});    
-    const elements = a.map((item, index,array) => { 
+  const monthItem = () => {  
+    const elements = myBe.map((item, index,array) => { 
       const { date, be, classN } = item;
       let dateItem = new Date(date);    
       let classNames = 'col cell-calendar'; 
@@ -57,68 +50,20 @@ const CalendarBox = ({myBe, myDate}) => {
  
   return (
   <div className="row mb-5">
-    <div className="col-sm-6">
-      <div className="row p-2">
-      <div className="col">{date[0].toLocaleString("ru", {month: 'long'}).toUpperCase()}</div>
-        <div className="w-100"></div>
-        <div className="col">Пн</div>
-        <div className="col">Вт</div>
-        <div className="col">Ср</div>
-        <div className="col">Чт</div>
-        <div className="col">Пт</div>
-        <div className="col">Сб</div>
-        <div className="col">Вс</div>
-        <div className="w-100"></div>
-        {monthItem(0)}
-      </div>
-    </div>
-    <div className="col-sm-6">
-    <div className="row p-2">
-      <div className="col">{date[1].toLocaleString("ru", {month: 'long'}).toUpperCase()}</div>
-        <div className="w-100"></div>
-        <div className="col">Пн</div>
-        <div className="col">Вт</div>
-        <div className="col">Ср</div>
-        <div className="col">Чт</div>
-        <div className="col">Пт</div>
-        <div className="col">Сб</div>
-        <div className="col">Вс</div>
-        <div className="w-100"></div>
-        {monthItem(1)}
-      </div>
-    </div>
-    <div className="col-sm-6">
-    <div className="row p-2">
-      <div className="col">{date[2].toLocaleString("ru", {month: 'long'}).toUpperCase()}</div>
-        <div className="w-100"></div>
-        <div className="col">Пн</div>
-        <div className="col">Вт</div>
-        <div className="col">Ср</div>
-        <div className="col">Чт</div>
-        <div className="col">Пт</div>
-        <div className="col">Сб</div>
-        <div className="col">Вс</div>
-        <div className="w-100"></div>
-        {monthItem(2)}
-      </div>
-    </div>
-    <div className="col-sm-6">
-    <div className="row p-2">
-      <div className="col">{date[3].toLocaleString("ru", {month: 'long'}).toUpperCase()}</div>
-        <div className="w-100"></div>
-        <div className="col">Пн</div>
-        <div className="col">Вт</div>
-        <div className="col">Ср</div>
-        <div className="col">Чт</div>
-        <div className="col">Пт</div>
-        <div className="col">Сб</div>
-        <div className="col">Вс</div>
-        <div className="w-100"></div>
-        {monthItem(3)}
-      </div>
-    </div>
-  </div>
-    
+    <div className="col">60 дней</div>
+    <div className="col">сегодня</div>
+    <div className="col">Последний день</div> 
+    <div className="w-100"></div>
+    <div className="col">Пн</div>
+    <div className="col">Вт</div>
+    <div className="col">Ср</div>
+    <div className="col">Чт</div>
+    <div className="col">Пт</div>
+    <div className="col">Сб</div>
+    <div className="col">Вс</div>
+    <div className="w-100"></div>
+    {monthItem()}
+      </div>    
   );
 }
 
